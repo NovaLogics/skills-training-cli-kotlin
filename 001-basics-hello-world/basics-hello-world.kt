3d12913f93fbb4ca0e67b4b290dad90d51562c90
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-03 17:47:59 +0530

    docs [ 001-section-2 ] : Doc complete

--- 001-basics-hello-world/basics-hello-world.kt
+++ 001-basics-hello-world/basics-hello-world.kt
@@ -1,4 +1,3 @@
-
 fun main() {
     println("_____________ SECTION 1 _____________")
 
@@ -7,13 +6,32 @@ fun main() {
 
     println("_____________________________________")
 
-    print("Hello, ") // Output: Hello (cursor stays on the same line)
-    println("world!") // Output: World (cursor moves to the next line)
+    print("Hello, ")
+    println("world!")
     // Output: Hello, world!
 
     println("_____________________________________")
+    println("_____________ SECTION 2 _____________")
+
+    // There are 5 boxes of popcorn
+    val popcorn = 5 
+
+    // There are 7 hotdogs
+    val hotdog = 7 
+
+    // There are 10 customers in the queue
+    var customers = 10 
+
+    // Some customers leave the queue
+    customers = 8
+
+    print("Customer Count : ")
+    println(customers)
+    // Output: Customer Count : 8
 }
 
+
+//Source : https://kotlinlang.org/docs/kotlin-tour-hello-world.html
 
 /*==========================
 SECTION 1 : Hello, world!
@@ -29,6 +47,25 @@ _____________________________
 
 `println()` adds a newline at the end of the output.
 `print()` does not add a newline; it keeps the cursor on the same line.
+
+ */
+
+ 
+/*==========================
+SECTION 2 : Variables
+============================
+- Mutable variables with `var`
+
+- Read-only variables with `val`
+    You can't change a read-only variable once you have given it a value.
+
+- To assign a value, use the assignment operator `=`
+
+- Variables can be declared outside the `main()` function at the beginning of your program. 
+    Variables declared in this way are said to be declared at `top level`.
+
+-* Kotlin recommend that you declare all variables as read-only (val) by default. 
+    Declare mutable variables (var) only if necessary.
 
  */
 
