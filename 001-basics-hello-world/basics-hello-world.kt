336f99e2cb824e4092a5f11757c4c0131b1025b8
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-03 17:39:47 +0530

    docs [ 001-section-1 ] : Doc complete

---
+++ 001-basics-hello-world/basics-hello-world.kt
@@ -0,0 +1,35 @@
+
+fun main() {
+    println("_____________ SECTION 1 _____________")
+
+    println("Hello, world!")
+    // Output: Hello, world!
+
+    println("_____________________________________")
+
+    print("Hello, ") // Output: Hello (cursor stays on the same line)
+    println("world!") // Output: World (cursor moves to the next line)
+    // Output: Hello, world!
+
+    println("_____________________________________")
+}
+
+
+/*==========================
+SECTION 1 : Hello, world!
+============================
+- `fun` is used to declare a function
+
+- The `main()` function is where your program starts from
+
+- The body of a function is written within curly braces `{}`
+_____________________________
+
+- `println()` and `print()` functions print their arguments to standard output
+
+`println()` adds a newline at the end of the output.
+`print()` does not add a newline; it keeps the cursor on the same line.
+
+ */
+
+
