4ab0db66000c15b8ddddc93ce51f08139e133e00
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:40:37 +0530

    docs [ 004-test-q3 ] : Doc complete

---
+++ 004-control-flow/practice-loops-test-3.kt
@@ -0,0 +1,16 @@
+/*==========================
+Test 3 :
+============================
+
+You have a list of words. 
+Use for and if to print only the words that start with the letter l.
+
+Hint
+Use the .startsWith() function for String type.
+----------------------
+*/
+
+fun main() {
+    val words = listOf("dinosaur", "limousine", "magazine", "language")
+    // Write your code here
+}
