8d0beb7d4720aea557d0abbac7024bc30ee0a09b
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:40:16 +0530

    docs [ 004-test-q1 ] : Doc complete

---
+++ 004-control-flow/practice-ce-test-1.kt
@@ -0,0 +1,26 @@
+/*==========================
+Test 1 : Conditional expressions practice
+============================
+
+Create a simple game where you win if throwing two dice results in the same number.
+Use if to print
+You win :) if the dice match
+or
+You lose :( otherwise.
+
+In this exercise, you import a package so that you can use the
+Random.nextInt() function to give you a random Int.
+
+Hint
+Use the equality operator (==) to compare the dice results.
+
+----------------------
+*/
+
+import kotlin.random.Random
+
+fun main() {
+    val firstResult = Random.nextInt(6)
+    val secondResult = Random.nextInt(6)
+    // Write your code here
+}
