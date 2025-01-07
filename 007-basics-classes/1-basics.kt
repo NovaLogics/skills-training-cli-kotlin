98bc5cd15e591b6f56bb42e80ef0225e5950f9b4
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 18:59:00 +0530

    docs [ 007-section-1 ] : Doc complete

---
+++ 007-basics-classes/1-basics.kt
@@ -0,0 +1,18 @@
+/* Classes in Kotlin
+
+- Kotlin supports object oriented programming using classes and objects.
+- Classes define a blueprint for creating objects, which can store data and behavior.
+- Objects are created based on the structure defined in the class.
+
+Declaring a Class
+    - Use the class keyword to define a class.
+        Example:
+        class Customer
+
+
+Why Use Classes?
+    - Efficiency: Avoid repeating code for objects with similar characteristics.
+    - Modularity: Group related data and functions together.
+    - Reusability: Define the behavior and properties once, and reuse them across multiple objects.
+*/
+class Customer()
