b49b0d2dc1939952c885cdf914f8d2f3141c5d07
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:16:00 +0530

    docs [ 002-test-q1 ] : Doc complete

--- 002-basics-data-types/practice-test-1.kt
+++ 002-basics-data-types/practice-test-1.kt
@@ -1,6 +1,9 @@
  /*==========================
 Test 1 
 ============================
+Explicitly declare the correct type for each variable:
+ */
+
 fun main() {
     val a: Int = 1000 
     val b = "log message"
@@ -8,14 +11,4 @@ fun main() {
     val d = 100_000_000_000_000
     val e = false
     val f = '\n'
-}
- */
-
- fun main() {
-    val a: Int = 1000 
-    val b: String = "log message"
-    val c: Double = 3.14
-    val d: Long = 100_000_000_000_000
-    val e: Boolean = false
-    val f: Char = '\n'
 }
