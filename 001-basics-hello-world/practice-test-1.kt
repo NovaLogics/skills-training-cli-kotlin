38b509b4f144ec18799d1224c872f82328d6e41e
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:14:10 +0530

    docs [ 001-test-a1 ] : Doc complete

--- 001-basics-hello-world/practice-test-1.kt
+++ 001-basics-hello-world/practice-test-1.kt
@@ -1,22 +1,13 @@
- /*==========================
+/*==========================
 Test 1 
 ============================
 Complete the code to make the program print 
 "Mary is 20 years old" 
 to standard output:
+ */
 
 fun main() {
     val name = "Mary"
     val age = 20
     // Write your code here
-}
-
- */
-
- fun main() {
-    val name = "Mary"
-    val age = 20
-
-    // Write your code here
-    println("$name is $age years old")
 }
