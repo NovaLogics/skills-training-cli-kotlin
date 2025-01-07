ade540da5417bcafdbf1d46dffea2df1d4f49ac9
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:44:34 +0530

    docs [ 005-test-q1 ] : Doc complete

--- 005-basics-functions/practice-functions-test-1.kt
+++ 005-basics-functions/practice-functions-test-1.kt
@@ -5,6 +5,7 @@ Test 1 :
 Write a function called circleArea that takes the radius of a circle 
 in integer format as a parameter and outputs the area of that circle.
 ----------------------
+*/
 
 import kotlin.math.PI
 
@@ -12,22 +13,4 @@ import kotlin.math.PI
 
 fun main() {
     println(circleArea(2))
-}
-
-*/
-
-import kotlin.math.PI
-
-// Write your code here
-
-fun circleArea(radius: Int): Double = PI * (radius * radius)
-
-//OR
-
-// fun circleArea(radius: Int): Double {
-//     return PI * radius * radius
-// }
-
-fun main() {
-    println(circleArea(2)) // 12.566370614359172
 }
