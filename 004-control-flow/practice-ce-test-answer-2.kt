b8c1b62fc960af3a914b05e1754f8fe4fe386456
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-07 20:41:08 +0530

    docs [ 004-test-a2 ] : Doc complete

---
+++ 004-control-flow/practice-ce-test-answer-2.kt
@@ -0,0 +1,38 @@
+/*==========================
+ Test 2
+ ============================
+Using a when expression, update the following program so that it prints 
+the corresponding actions when you input the names of game console buttons.
+
+Button      Action
+
+A           Yes
+B           No
+X           Menu
+Y           Nothing
+Other       There is no such button
+
+fun main() {
+    val button = "A"
+
+    println(
+        // Write your code here
+    )
+}
+
+ */
+
+
+ fun main() {
+    val button = "A"
+
+    println(
+        when(button){
+            "A" -> "Yes"
+            "B" -> "No"
+            "X" -> "Menu"
+            "Y" -> "Nothing"
+            else -> "There is no such button"
+        }
+    )
+}
